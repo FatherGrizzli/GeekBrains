cd5a0ffe8c9c3e1684e35a75bdadadb0b7a28f61
Your Name <grizzli.one@gmail.com>
2023-06-14 17:35:32 +0300

    task_1S

---
+++ C#/Seminars_1/Task_1S/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.WriteLine("Today is Friday");
+//Console.Write("Today is Friday");
+
+//Console.ReadLine();
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + number);
+Console.WriteLine($"your number is {number}");
+
+//int, double (float), bool
