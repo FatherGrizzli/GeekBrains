70d37760ff3d362d9ce3bfcfbe2151c225c1d85f
Your Name <grizzli.one@gmail.com>
2023-06-24 18:08:41 +0300

    HW4_TasK2

--- C#/lecture_2/Task_2/Program.cs
+++ C#/lecture_2/Task_2/Program.cs
@@ -1,11 +1,1 @@
-﻿int num = new Random().Next(10,1000);
-System.Console.WriteLine($"Твое число {num}");
-int a = num % 10;
-
-if (num > 100)
-{
-    Console.WriteLine($"Результат {a}");
-}
-else{
-    Console.WriteLine($"третьей цифры нет");
-}
+﻿
