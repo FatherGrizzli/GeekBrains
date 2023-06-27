4ec6499a7d288b175b904d4bc4fcddcb5254d2e4
Your Name <grizzli.one@gmail.com>
2023-06-27 10:28:31 +0300

    HWTASK2_3

--- C#/HW_LECTURE_4/Task_2/Program.cs
+++ C#/HW_LECTURE_4/Task_2/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите число");
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число");
 int num_1 = Convert.ToInt32(Console.ReadLine());
 int sum = SummaNum(num_1);
 Console.WriteLine($"Сумма цифр {num_1} равно: {SummaCount(num_1,sum)} ");
