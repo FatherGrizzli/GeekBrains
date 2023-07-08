40515663674bac754199dab57ce2387d62d9a1fa
Your Name <grizzli.one@gmail.com>
2023-07-08 19:12:22 +0300

    HW_9

---
+++ C#/HW9/TASK1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*void PrintNumbers(int n)
+    {
+        if (n < 0) PrintNumbers((n)-1);
+        Console.Write(n + " ");
+        
+    }
+PrintNumbers(10);
+Альтернативное решение но не смог развернуть массив так таковой 
+*/
+using System;
+
+class Program
+{
+    static void PrintNumbers(int n)
+    {
+        if (n == 1)
+        {
+            Console.Write(n);
+            return;
+        }
+        Console.Write(n + ", ");
+        PrintNumbers(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        PrintNumbers(N);
+    }
+}
---
+++ C#/HW9/TASK3/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
+*/
+
+//Альтернативное решение 
+using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите значение m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+
+        Console.WriteLine($"A({m},{n}) = {result}");
+    }
+}
