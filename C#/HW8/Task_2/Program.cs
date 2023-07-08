b4023e061e1f493107feaae6115888abe3e7ef00
Your Name <grizzli.one@gmail.com>
2023-07-08 19:24:05 +0300

    HW_8

---
+++ C#/HW8/Task_2/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 },
+            { 5, 2, 6, 7 }
+        };
+
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        int minSum = int.MaxValue;
+        int minSumRow = 0;
+
+        // Найти строку с наименьшей суммой элементов
+        for (int i = 0; i < rows; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                sum += array[i, j];
+            }
+
+            if (sum < minSum)
+            {
+                minSum = sum;
+                minSumRow = i;
+            }
+        }
+
+        Console.WriteLine($"Строка с наименьшей суммой элементов: {minSumRow + 1}");
+    }
+}
+*/
+//Альтернатива
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
