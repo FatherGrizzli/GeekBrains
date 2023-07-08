232147fc4e8e4333d53491f40d029dcd4819f7fb
Your Name <grizzli.one@gmail.com>
2023-07-08 19:18:46 +0300

    HW_9_pravka

--- C#/HW9/TASK2/Program.cs
+++ C#/HW9/TASK2/Program.cs
@@ -9,3 +9,24 @@ return n;
 else return m + summa(m + 1, n);
 }
 Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
+
+/*
+//Альтернатива 
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int SumOfElements(int n, int m)
+{
+  if (n == m) return n;
+  else return SumOfElements(n + 1, m) + n;
+}
+
+int n = Prompt("Input N: ");
+int m = Prompt("Input M: ");
+
+Console.WriteLine(SumOfElements(n, m));
+*/
